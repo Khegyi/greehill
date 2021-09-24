77f091f669d815b41cba2bcc2da8526946b05ed9
Khegyi <khegyi.balint@gmail.com>
2021-09-24 16:39:25 +0200

    Create and Render Grid & Handle CellClick functions

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import React, { useEffect, useState } from "react";
 import './App.css';
 
 function App() {
+
+  const [theGrid, setTheGrid] = useState([]);
+  const defaultStartingRowNumber = 32;
+  const defaultStartingCollNumber = 32;
+
+  
+  function createTheGrid () {
+  
+    let grid  = [];
+  
+    for (let i = 0; i < defaultStartingRowNumber; i++) {
+      for (let z = 0; z < defaultStartingCollNumber; z++) {
+          grid.push({row: i, coll: z, alive: false})
+      }
+    }
+    setTheGrid(grid);
+  }
+
+const renderTheGrid = () => {
+
+  let table = [];
+
+  for (let t = 0; t < defaultStartingRowNumber; t++) {
+    const eachRow = theGrid.filter(r =>  r.row === t)
+    .map(ren => {
+      return <div className="cell" data-cellnum={ren.coll} key={`${ren.row}${ren.coll}`}><button onClick={() => handleCellClick(ren)} className={(ren.alive ? "alive" : "dead")}>{(ren.alive ? "I" : "0")}</button></div>;
+    })
+    const rowRes = <div key={t} data-rownum={t} className="row">{eachRow}</div>
+    table.push(rowRes);
+  }
+
+return table;
+} 
+
+const handleCellClick = (data) => {
+  console.log(data);
+  const tempGrid = [...theGrid];
+  tempGrid.map(cell => {
+    if(cell.row === data.row && cell.coll === data.coll){
+     cell.alive = !data.alive;
+    }
+    return cell;
+  })
+
+  setTheGrid(tempGrid);
+} 
+
+  useEffect(() => {
+     createTheGrid();
+  }, []);
+
+
+  renderTheGrid();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Game of Life</h1>
       </header>
+      <div className="App-body">
+        <div className="Game-grid">
+          {renderTheGrid()}
+        </div>
+      </div>
     </div>
   );
 }
