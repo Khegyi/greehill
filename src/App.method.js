609e966194b692b2bf9f798d904d293ee523e708
Khegyi <khegyi.balint@gmail.com>
2021-09-29 22:17:27 +0200

    codecleaning

--- src/App.method.js
+++ src/App.method.js
@@ -1,10 +1,4 @@
-import _ from "underscore";
 export function isCellAlive(cellpos, grid, gridRowNum) {
-  /*  
-  const result = grid.find(
-    (c) => c.row === cellpos.row && c.coll === cellpos.coll
-  );
-  return result.alive; */
   const index = cellpos.row * gridRowNum + cellpos.coll;
   return grid[index].alive;
 }
@@ -29,48 +23,22 @@ export function getNeighboursPosition(cell, gridRowNum, griCollNum) {
 export function countAliveNeighbours(neighbours, grid, gridRowNum) {
   let counter = 0;
   let i = 0;
-  // console.log("length:", neighbours.length);
   while (i < neighbours.length) {
-    //  console.log(i);
-    //  console.log("i:", i, "counter", counter, neighbours[i]);
     const index = neighbours[i].row * gridRowNum + neighbours[i].coll;
     const res = grid[index].alive;
-    /*  console.log("res"); */
     if (res) {
-      //  console.log(res);
       counter++;
     }
     i++;
-    //   console.log(i);
     if (counter > 3) {
-      //  console.log("break", counter);
       break;
     }
   }
-  /*   neighbours.forEach((cell) => {
-    const index = cell.row * gridRowNum + cell.coll;
-    const res = grid[index].alive;
-    console.log("cs");
-    if (res) {
-      //  console.log(res);
-      counter++;
-    }
-    /*     while (counter < 4) {
-      
-
-      if (counter < 4) {
-        break;
-      }
-    } 
-  }); */
-  // console.log(counter);
   return counter;
 }
 
 export function willCellSurvive(cell, grid, gridRowNum, griCollNum) {
-  //console.log(cell);
   const aliveCounter = countAliveNeighbours(cell.neighbours, grid, gridRowNum);
-  // const aliveCounter = Math.floor(Math.random() * 8);
   if (cell.alive) {
     if (aliveCounter < 2 || aliveCounter > 3) {
       return false;
